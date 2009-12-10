d3ccbb0f957a01fb559716e8602ea333086cdbbf
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-12-10 15:51:01 +0000

    Moved the security advisors to the prefilter.

--- filter/src/main/java/uk/ac/ox/oucs/vle/OAuthPostFilter.java
+++ filter/src/main/java/uk/ac/ox/oucs/vle/OAuthPostFilter.java
@@ -2,9 +2,6 @@ package uk.ac.ox.oucs.vle;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -16,9 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.sakaiproject.authz.api.SecurityAdvisor;
-import org.sakaiproject.authz.cover.SecurityService;
-import org.sakaiproject.component.cover.ServerConfigurationService;
 import org.sakaiproject.event.cover.UsageSessionService;
 import org.sakaiproject.tool.cover.SessionManager;
 import org.sakaiproject.user.api.Authentication;
@@ -28,20 +22,14 @@ import org.sakaiproject.user.cover.UserDirectoryService;
 public class OAuthPostFilter implements Filter {
 
 	private final static Log log = LogFactory.getLog(OAuthPostFilter.class);
-	
-	// Can share this advisor between threads.
-	private SecurityAdvisor limitedPermissionAdvisor = null;
-	
-	private boolean debugPermissions = false;
-	
+
 	public void destroy() {
 
 	}
 
 	public void doFilter(ServletRequest servletRequest, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		CollectingPermissionAdvisor collector = null;
-		
+
 		if (servletRequest instanceof HttpServletRequest) {
 			HttpServletRequest request = (HttpServletRequest) servletRequest;
 			Principal principal = request.getUserPrincipal();
@@ -54,13 +42,6 @@ public class OAuthPostFilter implements Filter {
 						// TODO This is a hack and we should go through the AuthenticationManager API.
 						Authentication authentication = new org.sakaiproject.util.Authentication(userId, userEid);
 						UsageSessionService.login(authentication, request);
-						if (limitedPermissionAdvisor != null) {
-							SecurityService.pushAdvisor(limitedPermissionAdvisor);
-							if (debugPermissions) {
-								collector = new CollectingPermissionAdvisor();
-								SecurityService.pushAdvisor(collector);
-							}
-						}
 					} catch (UserNotDefinedException e) {
 						log.warn("Failed to find user. This shouldn't happen", e);
 					}
@@ -68,20 +49,10 @@ public class OAuthPostFilter implements Filter {
 			}
 		}
 		chain.doFilter(servletRequest, response);
-		
-		if (collector != null) {
-			log.info(collector);
-		}
 	}
 
 	public void init(FilterConfig filterConfig) throws ServletException {
-		String permissions = ServerConfigurationService.getString("oauth.permissions", null);
-		if (permissions != null) {
-			Set<String> permissionSet = new HashSet<String>(Arrays.asList(permissions.split(",")));
-			limitedPermissionAdvisor = new LimitedPermissionsAdvisor(permissionSet);
-			log.info(limitedPermissionAdvisor);
-		}
-		debugPermissions = ServerConfigurationService.getBoolean("oauth.permissions.debug", false);
+
 	}
 
 }
--- filter/src/main/java/uk/ac/ox/oucs/vle/OAuthPreFilter.java
+++ filter/src/main/java/uk/ac/ox/oucs/vle/OAuthPreFilter.java
@@ -3,6 +3,9 @@ package uk.ac.ox.oucs.vle;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,6 +16,12 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.authz.api.SecurityAdvisor;
+import org.sakaiproject.authz.cover.SecurityService;
+import org.sakaiproject.component.cover.ServerConfigurationService;
 
 import net.oauth.OAuthAccessor;
 import net.oauth.OAuthException;
@@ -28,58 +37,93 @@ import net.oauth.server.OAuthServlet;
  * 
  */
 public class OAuthPreFilter implements Filter {
+	
+	private static final Log log = LogFactory.getLog(OAuthPreFilter.class);
 
 	private OAuthProvider provider;
+
+	// Can share this advisor between threads.
+	private SecurityAdvisor limitedPermissionAdvisor = null;
+
+	private boolean debugPermissions = false;
 
 	public void destroy() {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
-			FilterChain chain) throws IOException, ServletException {
-		if (servletRequest instanceof HttpServletRequest && servletResponse instanceof HttpServletResponse) {
-			final HttpServletRequest request = (HttpServletRequest)servletRequest;
-			final HttpServletResponse response = (HttpServletResponse)servletResponse;
-            OAuthMessage requestMessage = OAuthServlet.getMessage(request, null);
+	public void doFilter(ServletRequest servletRequest,
+			ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+		if (servletRequest instanceof HttpServletRequest
+				&& servletResponse instanceof HttpServletResponse) {
+			final HttpServletRequest request = (HttpServletRequest) servletRequest;
+			final HttpServletResponse response = (HttpServletResponse) servletResponse;
+			OAuthMessage requestMessage = OAuthServlet
+					.getMessage(request, null);
 
-            boolean hasOAuth = requestMessage.getToken() != null;
-            if (hasOAuth) {
-    		try {
-            // TODO Need to check if we have any OAuth parameters in the request.
-            OAuthAccessor accessor = provider.getAccessor(requestMessage);
-            provider.validateMessage(requestMessage, accessor);
-            final String userId = (String) accessor.getProperty("user");
-            if (userId != null) {
-            	HttpServletRequest wrappedRequest = new HttpServletRequestWrapper(request) {
-            		public Principal getUserPrincipal() {
-            			return new Principal() {							
-							public String getName() {
-								return userId;
+			boolean hasOAuth = requestMessage.getToken() != null;
+			if (hasOAuth) {
+				try {
+					CollectingPermissionAdvisor collector = null;
+					// TODO Need to check if we have any OAuth parameters in the
+					// request.
+					OAuthAccessor accessor = provider
+							.getAccessor(requestMessage);
+					provider.validateMessage(requestMessage, accessor);
+					final String userId = (String) accessor.getProperty("user");
+					if (userId != null) {
+						HttpServletRequest wrappedRequest = new HttpServletRequestWrapper(
+								request) {
+							public Principal getUserPrincipal() {
+								return new Principal() {
+									public String getName() {
+										return userId;
+									}
+								};
 							}
-						}; 
-            		}
-            	};
-            	chain.doFilter(wrappedRequest, servletResponse);
-            } else {
-            	OAuthProblemException problem =  new OAuthProblemException("No authenticated User");
-            	problem.setParameter(HttpMessage.STATUS_CODE, 401);
-            	throw problem;
-            }
-			} catch (OAuthException oae) {
-				OAuthServlet.handleException(response, oae, null);
-			} catch (URISyntaxException e) {
-				OAuthServlet.handleException(response, e, null);
+						};
+
+						if (limitedPermissionAdvisor != null) {
+							SecurityService
+									.pushAdvisor(limitedPermissionAdvisor);
+						}
+						if (debugPermissions) {
+							collector = new CollectingPermissionAdvisor();
+							SecurityService.pushAdvisor(collector);
+						}
+						chain.doFilter(wrappedRequest, servletResponse);
+						
+						if (collector != null) {
+							log.info(collector);
+						}
+					} else {
+						OAuthProblemException problem = new OAuthProblemException(
+								"No authenticated User");
+						problem.setParameter(HttpMessage.STATUS_CODE, 401);
+						throw problem;
+					}
+				} catch (OAuthException oae) {
+					OAuthServlet.handleException(response, oae, null);
+				} catch (URISyntaxException e) {
+					OAuthServlet.handleException(response, e, null);
+				}
+			} else {
+				chain.doFilter(request, response);
 			}
-            } else {
-            	chain.doFilter(request, response);
-            }
-		
+
 		}
 	}
 
 	public void init(FilterConfig filterConfig) throws ServletException {
 		provider = OAuthProviderCover.getInstance();
+		String permissions = ServerConfigurationService.getString("oauth.permissions", null);
+		if (permissions != null) {
+			Set<String> permissionSet = new HashSet<String>(Arrays.asList(permissions.split(",")));
+			limitedPermissionAdvisor = new LimitedPermissionsAdvisor(permissionSet);
+			log.info(limitedPermissionAdvisor);
+		}
+		debugPermissions = ServerConfigurationService.getBoolean("oauth.permissions.debug", false);
 	}
 
 }
