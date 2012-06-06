7e657f0c1bf00ee1ec866297c31ecc7d96092548
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-06-06 11:34:29 +0100

    Create an hibernate implementation of the consumerDAO

---
+++ impl/dao-hbm/src/main/java/uk/ac/ox/oucs/oauth/dao/HibernateConsumerDao.java
@@ -0,0 +1,23 @@
+package uk.ac.ox.oucs.oauth.dao;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import uk.ac.ox.oucs.oauth.domain.Consumer;
+
+public class HibernateConsumerDao extends HibernateDaoSupport implements ConsumerDao {
+    public void create(final Consumer consumer) {
+        getHibernateTemplate().save(consumer);
+    }
+
+    public Consumer get(String consumerId) {
+        return (Consumer) getHibernateTemplate().get(Consumer.class, consumerId);
+    }
+
+    public Consumer update(Consumer consumer) {
+        getHibernateTemplate().update(consumer);
+        return consumer;
+    }
+
+    public void remove(Consumer consumer) {
+        getHibernateTemplate().delete(consumer);
+    }
+}
