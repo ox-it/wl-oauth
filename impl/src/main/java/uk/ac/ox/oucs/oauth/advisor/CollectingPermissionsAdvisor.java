fe12da00d1db40b2e25c186d1228793ff0ce76ac
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-06-06 12:27:12 +0100

    Update the permissions advisor to update rights

--- impl/src/main/java/uk/ac/ox/oucs/oauth/advisor/CollectingPermissionsAdvisor.java
+++ impl/src/main/java/uk/ac/ox/oucs/oauth/advisor/CollectingPermissionsAdvisor.java
@@ -1,27 +1,40 @@
 package uk.ac.ox.oucs.oauth.advisor;
 
 import org.sakaiproject.authz.api.SecurityAdvisor;
-
-import java.util.HashSet;
-import java.util.Set;
+import uk.ac.ox.oucs.oauth.dao.ConsumerDao;
+import uk.ac.ox.oucs.oauth.domain.Consumer;
 
 /**
- * Advisor used during test phases to determine which permissions must be enabled for the service
+ * Advisor used during record phase to determine which permissions must be enabled for the service
  * <p>
- * Every permission used during one request will be collected to be read later.
+ * Every permission used during one request will be enabled automatically.
  * </p>
  *
  * @author Colin Hebert
  */
 public class CollectingPermissionsAdvisor implements SecurityAdvisor {
-    private Set<String> permissions = new HashSet<String>();
+    private ConsumerDao consumerDao;
+    private Consumer consumer;
+
+    public CollectingPermissionsAdvisor(ConsumerDao consumerDao, Consumer consumer) {
+        this.consumerDao = consumerDao;
+        this.consumer = consumer;
+    }
 
     public SecurityAdvice isAllowed(String userId, String function, String reference) {
-        permissions.add(function);
+        if (!consumer.getRights().contains(function)) {
+            try {
+                consumer.getRights().add(function);
+                consumerDao.update(consumer);
+            } catch (Exception e) {
+                //If the update doesn't work, carry on
+                //TODO: Log this exception
+            }
+        }
         return SecurityAdvice.PASS;
     }
 
     public String toString() {
-        return "Collected permissions: " + permissions;
+        return "Permission collector";
     }
 }
