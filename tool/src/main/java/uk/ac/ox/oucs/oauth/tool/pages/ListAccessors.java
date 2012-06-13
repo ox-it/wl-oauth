9dd6d4a59c05d07fc7da269565dda01bd8d826b0
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-06-13 10:55:32 +0100

    Change tool to use wicket instead of JSF

---
+++ tool/src/main/java/uk/ac/ox/oucs/oauth/tool/pages/ListAccessors.java
@@ -0,0 +1,55 @@
+package uk.ac.ox.oucs.oauth.tool.pages;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.sakaiproject.tool.api.SessionManager;
+import uk.ac.ox.oucs.oauth.domain.Accessor;
+import uk.ac.ox.oucs.oauth.domain.Consumer;
+import uk.ac.ox.oucs.oauth.service.OAuthService;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author Colin Hebert
+ */
+public class ListAccessors extends SakaiPage {
+    @SpringBean
+    private SessionManager sessionManager;
+    @SpringBean
+    private OAuthService oAuthService;
+
+    public ListAccessors() {
+        String userId = sessionManager.getCurrentSessionUserId();
+        Collection<Accessor> accessors = oAuthService.getAccessAccessorForUser(userId);
+        add(new ListView<Accessor>("accessorlist", new ArrayList<Accessor>(accessors)) {
+            @Override
+            protected void populateItem(ListItem<Accessor> components) {
+                final Consumer consumer = oAuthService.getConsumer(components.getModelObject().getConsumerId());
+                DateFormat dateFormat = DateFormat.getDateInstance();
+                components.add(new ExternalLink("consumerUrl", consumer.getURL(), consumer.getName()));
+                components.add(new Label("consumerDescription", consumer.getDescription()));
+                components.add(new Label("creationDate", dateFormat.format(components.getModelObject().getCreationDate())));
+                components.add(new Label("expirationDate", dateFormat.format(components.getModelObject().getExpirationDate())));
+
+                components.add(new Link<Accessor>("delete", components.getModel()) {
+                    @Override
+                    public void onClick() {
+                        try {
+                            oAuthService.revokeAccessor(getModelObject().getToken());
+                            setResponsePage(getPage().getClass());
+                            getSession().info(consumer.getName() + "' token has been removed.");
+                        } catch (Exception e) {
+                            warn("Couldn't remove '" + consumer.getName() + "'s token': " + e.getLocalizedMessage());
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ tool/src/main/java/uk/ac/ox/oucs/oauth/tool/pages/SakaiPage.java
@@ -0,0 +1,146 @@
+package uk.ac.ox.oucs.oauth.tool.pages;
+
+import org.apache.wicket.*;
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.feedback.FeedbackMessage;
+import org.apache.wicket.feedback.IFeedbackMessageFilter;
+import org.apache.wicket.markup.html.IHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.tool.api.SessionManager;
+
+/**
+ * @author Colin Hebert
+ */
+public abstract class SakaiPage extends WebPage implements IHeaderContributor {
+    @SpringBean
+    private ServerConfigurationService serverConfigurationService;
+    @SpringBean
+    private SiteService siteService;
+    @SpringBean
+    private SessionManager sessionManager;
+    private RepeatingView menu;
+
+    protected SakaiPage() {
+        init();
+    }
+
+    protected SakaiPage(IModel<?> model) {
+        super(model);
+        init();
+    }
+
+    protected SakaiPage(IPageMap pageMap) {
+        super(pageMap);
+        init();
+    }
+
+    protected SakaiPage(IPageMap pageMap, IModel<?> model) {
+        super(pageMap, model);
+        init();
+    }
+
+    protected SakaiPage(PageParameters parameters) {
+        super(parameters);
+        init();
+    }
+
+    protected SakaiPage(IPageMap pageMap, PageParameters parameters) {
+        super(pageMap, parameters);
+        init();
+    }
+
+    private void init() {
+        FeedbackPanel feedbackPanel = new FeedbackPanel("feedback") {
+            @Override
+            protected Component newMessageDisplayComponent(final String id, final FeedbackMessage message) {
+                if (message.getLevel() == FeedbackMessage.INFO)
+                    add(new SimpleAttributeModifier("class", "success"));
+                else
+                    add(new SimpleAttributeModifier("class", "alertMessage"));
+
+                return super.newMessageDisplayComponent(id, message);
+            }
+
+            // If we don't link up visibility to having messages, then when changing the filter after displaying
+            // the message, the message disappears but they surrounding box remains.
+            public boolean isVisible() {
+                return anyMessage();
+            }
+        };
+        feedbackPanel.setFilter(new IFeedbackMessageFilter() {
+            public boolean accept(FeedbackMessage message) {
+                return !message.isRendered();
+            }
+        });
+        add(feedbackPanel);
+        menu = new RepeatingView("menu");
+        add(menu);
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        // get Sakai skin
+        String skinRepo = serverConfigurationService.getString("skin.repo");
+        String skin = siteService.findTool(sessionManager.getCurrentToolSession().getPlacementId()).getSkin();
+        if (skin == null) {
+            skin = serverConfigurationService.getString("skin.default");
+        }
+        String toolCSS = skinRepo + "/" + skin + "/tool.css";
+        String toolBaseCSS = skinRepo + "/tool_base.css";
+
+        // Sakai additions
+        response.renderJavascriptReference("/library/js/headscripts.js");
+        response.renderCSSReference(toolBaseCSS);
+        response.renderCSSReference(toolCSS);
+        response.renderOnLoadJavascript("\nif (typeof setMainFrameHeight !== 'undefined'){\nsetMainFrameHeight( window.name );\n}");
+
+        // Tool additions (at end so we can override if required)
+        response.renderString("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n");
+        response.renderCSSReference(new ResourceReference(getClass(), "style.css"));
+    }
+
+    /**
+     * Add a menu entry linking to a class page.
+     * <p>
+     * Automatically disable the link if it's to the current page.
+     * </p>
+     *
+     * @param clazz classPage
+     * @param text  Link's text
+     * @param title Title attribute for the link
+     */
+    protected void addMenuLink(Class<? extends Page> clazz, IModel<String> text, String title) {
+        Link<Page> link = new BookmarkablePageLink<Page>("menuItem", clazz);
+        link.setEnabled(!getClass().equals(clazz));
+        addMenuLink(link, text, title);
+    }
+
+    /**
+     * Add a menu entry with a custom link
+     *
+     * @param link  Link to add to the menu bar
+     * @param text  Link's text
+     * @param title Title attribute for the link
+     */
+    protected void addMenuLink(final Link<Page> link, IModel<String> text, String title) {
+        WebMarkupContainer parent = new WebMarkupContainer(menu.newChildId());
+        menu.add(parent);
+        link.add(new Label("menuItemText", text).setRenderBodyOnly(true));
+        if (title != null)
+            link.add(new AttributeModifier("title", true, new ResourceModel(title)));
+
+        parent.add(link);
+    }
+}
