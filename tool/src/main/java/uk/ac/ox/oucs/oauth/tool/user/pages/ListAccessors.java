37e5530361aa9933374c38a95cc6de8e48059a49
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-08-16 16:55:08 +0100

    Open the consumer link in a pop-up instead of the site

--- tool/src/main/java/uk/ac/ox/oucs/oauth/tool/user/pages/ListAccessors.java
+++ tool/src/main/java/uk/ac/ox/oucs/oauth/tool/user/pages/ListAccessors.java
@@ -37,7 +37,7 @@ public class ListAccessors extends SakaiPage {
                 try {
                     final Consumer consumer = oAuthService.getConsumer(components.getModelObject().getConsumerId());
                     ExternalLink consumerHomepage = new ExternalLink("consumerUrl", consumer.getURL(), consumer.getName());
-                    consumerHomepage.add(new SimpleAttributeModifier("target", "_parent"));
+                    consumerHomepage.add(new SimpleAttributeModifier("target", "_blank"));
                     consumerHomepage.setEnabled(consumer.getURL() != null && !consumer.getURL().isEmpty());
                     components.add(consumerHomepage);
                     components.add(new Label("consumerDescription", consumer.getDescription()));
